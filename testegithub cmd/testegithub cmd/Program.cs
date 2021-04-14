bfa26992438c14370bdd42ba29e04fdbaf96d5f2
nathan-torresz <81251140+nathan-torresz@users.noreply.github.com>
2021-04-14 20:02:55 -0300

    Projeto VS criado

---
+++ testegithub cmd/testegithub cmd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testegithub_cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
