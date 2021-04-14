5b7ad91cc8aa0d06291423f9ae6366c3472d0830
nathan-torresz <81251140+nathan-torresz@users.noreply.github.com>
2021-04-14 20:43:04 -0300

    Alteraçao

--- testegithub cmd/testegithub cmd/Program.cs
+++ testegithub cmd/testegithub cmd/Program.cs
@@ -6,7 +6,7 @@ namespace testegithub_cmd
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bom Dia");
+            Console.WriteLine("Bom Noite");
         }
     }
 }
