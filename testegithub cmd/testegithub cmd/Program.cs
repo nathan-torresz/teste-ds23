75e616a6f7806cf1de0484d962b6d1ca17108e27
nathan-torresz <81251140+nathan-torresz@users.noreply.github.com>
2021-04-14 22:17:42 -0300

    Mais de 3 alterações

--- testegithub cmd/testegithub cmd/Program.cs
+++ testegithub cmd/testegithub cmd/Program.cs
@@ -6,7 +6,7 @@ namespace testegithub_cmd
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bom Noite");
+            Console.WriteLine("Concluido com sucesso a atividade!");
         }
     }
 }
